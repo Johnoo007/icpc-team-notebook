3eb7dca287bfc8d680481c176f7b8d2deca2307f
kwaipeuak. <psuwit.john.won@gmail.com>
2026-05-12 03:19:25 +0700

    Add test.cpp and new template; remove old
    Add a practice solution and introduce a new C++ template while deleting the outdated default template. practice/test.cpp: new simple sum program using fast I/O and common typedefs. templates/template.cpp: new base template with fastio, common type aliases, and a solve loop (prints "Hello, ICPC!" by default). Removed templates/default_template.cpp to replace the previous template file.

---
+++ templates/template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pipii = pair<int, pii>; 
+using tiii = tuple<int, int, int>; 
+
+#define all(x) (x).begin(), (x).end()
+#define fastio() ios::sync_with_stdio(0); cin.tie(0);
+
+void solve() {
+    // Write solution here
+    cout << "Hello, ICPC!" << endl;
+}
+
+int main() {
+    fastio();
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    return 0;
+}
