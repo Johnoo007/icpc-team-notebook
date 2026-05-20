cb1a15a746247b31536723fd0932c8e9dbaf9cce
Mushroom-code <psuwit.john.won@gmail.com>
2026-05-20 16:33:15 +0700

    20/5 : 1/2

---
+++ practice/Bronze/Complete Search/usaco_2017feb_b2_Why_Did_The_Cow_Cross_The_Road_II_Nine.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pipii = pair<int, pii>; 
+using tiii = tuple<int, int, int>; 
+
+#define all(x) (x).begin(), (x).end()
+#define fastio() ios::sync_with_stdio(0); cin.tie(0);
+
+void solve() {
+    // Write solution here
+    cout << "Hello, ICPC!" << endl;
+}
+
+int main() {
+    fastio();
+
+    // freopen("tttt.in", "r", stdin);
+    // freopen("tttt.out", "w", stdout);
+
+    
+
+
+    return 0;
+}
