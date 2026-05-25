da6c1739cb5bb5d24844851ed3e407a06498bff0
kwaipeuak. <psuwit.john.won@gmail.com>
2026-05-26 00:35:13 +0700

    Update Convention_john.cpp

--- practice/Silver/Sorting & Searching/Binary Search/Convention_john.cpp
+++ practice/Silver/Sorting & Searching/Binary Search/Convention_john.cpp
@@ -9,17 +9,53 @@ using tiii = tuple<int, int, int>;
 #define all(x) (x).begin(), (x).end()
 #define fastio() ios::sync_with_stdio(0); cin.tie(0);
 
-void solve() {
-    // Write solution here
-    cout << "Hello, ICPC!" << endl;
+int N,M,C;
+vector<ll> cow;
+
+bool solve(ll mid) {
+    int bus = 0;
+    int i = 0;
+
+    while (i < N) {
+        bus++;
+        int first = i; 
+        int cnt = 0;
+
+        while (i < N && cnt < C && cow[i] - cow[first] <= mid) {
+            i++;
+            cnt++;
+        }
+    }
+    if(bus <= M) return true;
+    return false;
 }
 
 int main() {
     fastio();
-    int t = 1;
-    // freopen("tttt.in", "r", stdin);
-    // freopen("tttt.out", "w", stdout);
-    // cin >> t;
-    while (t--) solve();
+    freopen("convention.in", "r", stdin);
+    freopen("convention.out", "w", stdout);
+    cin >> N >> M >> C;
+
+    for(int i = 0 ; i < N ; i++){
+        ll temp;
+        cin >> temp;
+        cow.push_back(temp);
+    }
+    sort(all(cow));
+
+    ll right = pow(10,9);
+    ll left = 0;
+    while (right > left)
+    {
+        ll mid = (right-left)/2 + left;
+        if(solve(mid)){
+            right = mid;
+        }else{
+            left = mid+1;
+        }
+    }
+
+    cout << right;
+    
     return 0;
 }
